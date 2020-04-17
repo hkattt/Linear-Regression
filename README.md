13f150e752314691e99b922f1fdbfae662e419d4
hkattt <2690133@schoolsnet.act.edu.au>
2020-04-17 14:10:29 +1000

    fixed some bugs, documentation, and got some test data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Linear Regression
-Linear regression using gradient descent
+Linear regression using gradient descent.
+
+References / Inspiration
+Menon, A., 2018. Linear Regression Using Gradient Descent. [online] towardsdatascience. Available at: <https://towardsdatascience.com/linear-regression-using-gradient-descent-97a6c8700931> [Accessed 17 April 2020]. 
--- main.py
+++ main.py
@@ -3,41 +3,62 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# setting the figures size
 plt.rcParams['figure.figsize'] = (12.0, 9.0)
 
-# reading in data from the csv file
-data = pd.read_csv('data.csv')
-# stores all the input data in a n * 1 dimensional vector
-X = data.iloc[:, 0]
-# stores all the expected outputs in a n * 1 dimensional vector
-Y = data.iloc[:, 1]
-# plots raw data
-plt.scatter(X, Y)
-plt.show()
+class LinearRegression():
+    def __init__(self):
+        # reading in data from the csv file
+        self.data = pd.read_csv("data_2.csv")
+        # stores all the input data in a n * 1 dimensional vector
+        self.X = self.data.iloc[:, 0]
+        # stores all the expected outputs in a n * 1 dimensional vector
+        self.Y = self.data.iloc[:, 1]
+        # initialise theta values to 0
+        self.theta_1 = 0
+        self.theta_0 = 0
 
-# initialise theta values to 0
-theta_1 = 0
-theta_0 = 0
+        self.alpha = 0.01  # learning Rate
+        self.iters = 1000  # number of iterations to perform gradient descent
 
-alpha = 0.1  # learning Rate
-iters = 1000  # number of iterations to perform gradient descent
+        self.m = float(len(self.X)) # number of training examples
 
-m = float(len(X)) # number of training examples
+    def plot_raw(self):
+        """ Plots raw data """
+        plt.scatter(self.X, self.Y) # plots all the data points
+        # axis labels / title
+        plt.title("Years of Experience vs Salary")
+        plt.xlabel("Years of Experience")
+        plt.ylabel("Salary ($)")
+        # displays figure
+        plt.show()
 
-# gradient descent algorithm
-for i in range(iters): 
-    Y_pred = theta_1 * X + theta_0  # The current predicted value of Y
-    D_1 = (1 / m) * sum((Y_pred - Y) * X)  # Derivative wrt m
-    D_0 = (1 / m) * sum(Y_pred - Y)  # Derivative wrt c
-    theta_1 = theta_1 - alpha * D_1  # Update m
-    theta_0 = theta_0 - alpha * D_0  # Update c
+    def gradient_descent(self):
+        """ Performs gradient descent """
+        # iterates over all the training iterations (epochs)
+        for _ in range(self.iters): 
+            h = self.theta_1 * self.X + self.theta_0  # hypothesis
+            D_1 = (1 / self.m) * sum((h - self.Y) * self.X)  # partial derivative with respect to theta 1
+            D_0 = (1 / self.m) * sum(h - self.Y)  # partial derivative with respect to theta 0
+            self.theta_1 = self.theta_1 - self.alpha * D_1  # update theta 1
+            self.theta_0 = self.theta_0 - self.alpha * D_0  # update theta 0
 
-# computes values for theta 1 and theta 0
-print(theta_1, theta_0)
+    def plot(self):
+        """ Plots the raw data with the regression line """
+        h = self.theta_1 * self.X + self.theta_0 # hypothesis function
+        plt.scatter(self.X, self.Y) # raw data
+        plt.plot([min(self.X), max(self.X)], [min(h), max(h)], label="Regression Line", color="red")  # regression line (hypothesis function)
+        # axis labels / title
+        plt.title("Years of Experience vs Salary")
+        plt.xlabel("Years of Experience")
+        plt.ylabel("Salary ($)")
+        plt.legend()
+        # displays figure
+        plt.show()
 
-# plotting the line of best fit (prediction)
-Y_pred = theta_1 * X + theta_0
-
-plt.scatter(X, Y) # raw data
-plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red')  # regression line
-plt.show()
+linear_regression = LinearRegression()
+linear_regression.plot_raw()
+linear_regression.gradient_descent()
+print(linear_regression.theta_1, linear_regression.theta_0)
+linear_regression.plot()
